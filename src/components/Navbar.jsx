aeefaca395392877a60d10371a9eeeabadab6aaf
VinaySingh1045 <vy0891547@gmail.com>
2024-07-21 17:29:17 +0530

    Update-Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,13 @@ const Navbar = () => {
                     <span className='text-green-500'>&lt;</span>
                     Pass
                     <span className='text-green-500'>OP/&gt;</span>
-                    
+
                 </div>
-                
-                <button className="github rounded-full bg-green-500 px-3 py-2 flex items-center gap-1 font-bold">
-                    <img className='' src="/github.svg" alt="" />
-                GitHub</button>
+                <a href="https://github.com/VinaySingh1045" target='_blank'>
+                    <button className="github rounded-full bg-green-500 px-3 py-2 flex items-center gap-1 font-bold">
+                        <img className='' src="/github.svg" alt="" />
+                        GitHub</button>
+                </a>
             </div>
         </nav>
     )
